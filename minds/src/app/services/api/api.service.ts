d93b414efae3c0d3ae0aecbff0a81529903c4ce0
Clarence Castillo <clarencecastillo@outlook.com>
2018-10-12 15:51:34 +0800

    Implement api calls

--- minds/src/app/services/api/api.service.ts
+++ minds/src/app/services/api/api.service.ts
@@ -9,12 +9,25 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   registerClient(name, age, location, interests) {
-    this.http.post('http://127.0.0.1:8000/chatbotResult', {
+    return this.http.post('http://127.0.0.1:8000/chatbotResult', {
       userData: {
         name,
         age,
         location,
         interests
+      }
+    }).toPromise();
+  }
+
+  getPendingResume() {
+    return this.http.get('http://127.0.0.1:8000/jobcoach/pendingresumes/')
+      .toPromise();
+  }
+
+  confirmResume(name) {
+    return this.http.get('http://127.0.0.1:8000/jobcoach/confirmingResume', {
+      params: {
+        resumeName: name
       }
     }).toPromise();
   }
