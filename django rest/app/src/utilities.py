1672af48698349a12071932af8da96aeb9762c94
Brittany Goh <brittanyms.goh@gmail.com>
2018-10-12 11:34:52 +0800

    for joel

--- django rest/app/src/utilities.py
+++ django rest/app/src/utilities.py
@@ -7,5 +7,61 @@ def initializeData(requests):
             "interest" : ['singing', 'cooking'],
             "address" : 'Victoria Street 81',
             "work schedule" : "part-time"
-        }
+        },
+        "john": {
+            "name" : "john",
+            "interest" : ['dancing', 'running'],
+            "address" : 'Ang Mo Kio Avenue 3',
+            "work schedule" : "full time"
+        },   
+        "emily": {
+            "name" : "emily",
+            "interest" : ['dancing', 'singing'],
+            "address" : '11 Toa Payoh Rise',
+            "work schedule" : "full time"
+        },  
+        "amy": {
+            "name" : "amy",
+            "interest" : ['cooking', 'baking'],
+            "address" : '79 Serangoon Road',
+            "work schedule" : "part-time"
+        },       
+        "layla": {
+            "name" : "layla",
+            "interest" : ['eating', 'baking'],
+            "address" : '79 Clementi Road',
+            "work schedule" : "part-time"
+        },           
     }
+    requests.session['confirmed_resume'] = {
+        "britney": {
+            "name" : "britney",
+            "interest" : ['cooking', 'gardening'],
+            "address" : '9 Jurong East Road',
+            "work schedule" : "part-time"
+        },
+        "roy": {
+            "name" : "roy",
+            "interest" : ['gardening'],
+            "address" : '7 Ang Mo Kio',
+            "work schedule" : "full time"
+        },   
+        "joe": {
+            "name" : "joe",
+            "interest" : ['dancing', 'singing'],
+            "address" : '11 Toa Payoh Rise',
+            "work schedule" : "full time"
+        },  
+        "don": {
+            "name" : "don",
+            "interest" : ['eating', 'sleeping'],
+            "address" : '79 Serangoon Road',
+            "work schedule" : "part-time"
+        },       
+        "jen": {
+            "name" : "jen",
+            "interest" : ['eating', 'baking'],
+            "address" : '79 Clementi Road',
+            "work schedule" : "part-time"
+        },           
+    }    
